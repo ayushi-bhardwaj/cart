1672d5aee78225667af4c7c22f8d4086a4a134ef
unknown <ayushibhardwaj288@gmail.com>
2020-07-15 22:47:47 +0530

    cart

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function App() {
+  const name='ayushi'
+  
+  
+ 
+  return (
+    <div className="App">
+      <h1>Cart</h1>
+    </div>
+  );
+  }
+
+
+export default App;
